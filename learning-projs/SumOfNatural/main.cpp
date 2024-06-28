b30370b2184c9c6b7dff8f5e12e08fddd1bbd8bb
gabriel-05-dev <g.luchita@protonmail.com>
2024-06-28 13:47:11 +0100

    added user range input, finished mini task

--- learning-projs/SumOfNatural/main.cpp
+++ learning-projs/SumOfNatural/main.cpp
@@ -6,12 +6,20 @@ int sumOf(int start, int end, int sum = 0) {
         start++;
         sumOf(start, end, sum);
     } else {
-        std::cout << sum << std::endl;
-        return sum;
+    std::cout << sum << std::endl;
     }
-    return 0;
+    return 0; //successful
 }
 
 int main() {
+    int num_1;
+    int num_2;
+
+    std::cout << "Enter start : " << std::endl;
+    std::cin >> num_1;
+    std::cout << "Enter end : " << std::endl;
+    std::cin >> num_2;
     
+    std::cout << "The sum of numbers from " << num_1 << " to " << num_2 << " is " << sumOf(num_1, num_2) << std::endl;
+    return 0;
 }
